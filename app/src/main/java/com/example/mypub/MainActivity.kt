2bdd92f83f7e9e9e7486a2bd8b974683ae20d725
MKOLT\milos <mkoltonowski@edu.cdv.pl>
2023-10-22 11:24:41 +0200

    feat(#0001): init project; added google maps handling;

---
+++ app/src/main/java/com/example/mypub/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.mypub
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView, mapFragment)
+            .commit()
+
+        mapFragment.getMapAsync(this)
+    }
+
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.d("MapDebug", "onMapReady called")
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(52.43339094662937, 16.937463959198137))
+                .title("Marker")
+        )
+        val point : CameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(52.43339094662937, 16.937463959198137), 16F)
+
+        googleMap.moveCamera(point)
+        googleMap.animateCamera(point)
+
+        val toast = Toast.makeText(applicationContext, "Chuj ci w dupę", Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+}
